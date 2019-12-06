76668433542cfe04625cdf7a0ce963b40e6b1165
unknown <alisaliim85@gmail.com>
2019-12-06 20:59:36 +0300

    عمل الخدمات و تسجيل الدخول مع اخذ التوكن

--- ZwajApp-SPA/src/app/Nav/Nav.component.ts
+++ ZwajApp-SPA/src/app/Nav/Nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-Nav',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Nav.component.css']
 })
 export class NavComponent implements OnInit {
+  model: any = {};
 
-  constructor() { }
+  constructor(private authSrivce:AuthService) { }
 
   ngOnInit() {
   }
+
+  login(){
+    this.authSrivce.login(this.model).subscribe(
+      next=>{console.log('log in sucess')},
+      error=>{console.log('error')}
+    )
+    
+  };
 
 }
--- ZwajApp-SPA/src/app/app.module.ts
+++ ZwajApp-SPA/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './Nav/Nav.component';
+import { AuthService } from './_services/auth.service';
 
 @NgModule({
    declarations: [
@@ -15,10 +17,11 @@ import { NavComponent } from './Nav/Nav.component';
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
       
    ],
-   providers: [],
+   providers: [AuthService],
    bootstrap: [
       AppComponent
    ]
